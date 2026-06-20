7d2fd97b718701dd4f1163a9e51bf7e87af0e79a
antoniotrauthmann <antonio.trauthmanpmw@gmail.com>
2026-06-20 20:14:17 -0300

    Fase 3 Completa
    Classe BoardException implementada;
    refatorações;
    codificação da logica de movimentação integrada a dinamica do terminal

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,45 +1,57 @@
 package boardgame;
 
 public class Board {
+
     private int rows;
     private int columns;
     private Piece[][] pieces;
 
     public Board(int rows, int columns) {
         if (rows < 1 || columns < 1) {
-            throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
+            throw new BoardException("Erro ao criar tabuleiro: deve haver pelo menos 1 linha e 1 coluna");
         }
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
     }
 
-    public int getRows() { return rows; }
-    public int getColumns() { return columns; }
+    public int getRows() {
+        return rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
 
     public Piece piece(int row, int column) {
         if (!positionExists(row, column)) {
-            throw new BoardException("Position not on the board");
+            throw new BoardException("Posicao nao existente no tabuleiro");
         }
         return pieces[row][column];
     }
 
     public Piece piece(Position position) {
-        validatePosition(position);
+        if (!positionExists(position)) {
+            throw new BoardException("Posicao nao existente no tabuleiro");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
 
     public void placePiece(Piece piece, Position position) {
         if (thereIsAPiece(position)) {
-            throw new BoardException("There is already a piece on position " + position);
+            throw new BoardException("Ya existe uma peca na posicao " + position);
         }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
     }
 
     public Piece removePiece(Position position) {
-        validatePosition(position);
-        if (piece(position) == null) return null;
+        if (!positionExists(position)) {
+            throw new BoardException("Posicao nao existente no tabuleiro");
+        }
+        if (piece(position) == null) {
+            return null;
+        }
         Piece aux = piece(position);
         aux.position = null;
         pieces[position.getRow()][position.getColumn()] = null;
@@ -55,13 +67,9 @@ public class Board {
     }
 
     public boolean thereIsAPiece(Position position) {
-        validatePosition(position);
-        return piece(position) != null;
-    }
-
-    private void validatePosition(Position position) {
         if (!positionExists(position)) {
-            throw new BoardException("Position not on the board");
+            throw new BoardException("Posicao nao existente no tabuleiro");
         }
+        return piece(position) != null;
     }
 }
